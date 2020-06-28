b4e1ce32d0dc68f194e40a1bc9a4e36be2fce628
Sidney Goossens <sidney.goossens96@gmail.com>
2020-06-28 23:18:51 +0200

    8-using time.deltatime

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    //properties
+    Rigidbody rb;
+    public int rcsThrust;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    /**
+     * Handles key presses.
+     * @author Sidney Goossens
+     */
+    private void ProcessInput()
+    {
+        //thrust
+        if(Input.GetKey(KeyCode.Space))
+        {
+            print("Space pressed");
+            rb.AddRelativeForce(Vector3.up);
+        } 
+        
+        //rotation
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("D pressed");
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+}
